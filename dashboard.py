5fb1e538946e0adadf937acf64c0eabebf3eb5be
Jimmy <liujame@oregonstate.edu>
2025-04-06 00:12:03 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,33 +1,31 @@
 import os
 import argparse
-import dash  # (version 1.12.0) pip install dash
+import dash  # Recommend updating to a newer version, e.g., >=2.0.0
 import dash_bootstrap_components as dbc
 
 # Source guide: Callbacks layout separation (https://community.plotly.com/t/dash-callback-in-a-separate-file/14122/16)
 from dashboard_app.layout import app_layout
 from dashboard_app.callbacks import register_callbacks
 
-# app = dash.Dash(__name__)
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+# Initialize Dash app with Bootstrap theme
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Docker support
-parser = argparse.ArgumentParser()
-parser.add_argument("--docker", help="Change the default server host to 0.0.0.0", action='store_true')
+parser = argparse.ArgumentParser(description="TOS Options Dashboard with Polygon API")
+parser.add_argument("--docker", help="Change the default server host to 0.0.0.0 for Docker", action='store_true')
 args = parser.parse_args()
 
-# API credentials 
+# API credentials
 API_KEY = os.environ.get('POLYGON_API_KEY')
+if not API_KEY:
+    raise ValueError("POLYGON_API_KEY environment variable is not set. Please provide a valid Polygon API key.")
 
-# ------------------------------------------------------------------------------
 # App layout
 app.layout = app_layout
 
-# ------------------------------------------------------------------------------
-# Connect the Plotly graphs with Dash Components
+# Connect Plotly graphs with Dash components
 register_callbacks(app, API_KEY)
 
 if __name__ == '__main__':
-    if args.docker:
-        app.run_server(host='0.0.0.0', debug=True)
-    else:
-        app.run_server(debug=True)
+    host = '0.0.0.0' if args.docker else '127.0.0.1'
+    app.run_server(host=host, debug=True, port=8050)
